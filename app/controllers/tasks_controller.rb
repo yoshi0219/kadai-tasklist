2885d5714c6ea6b1b3873e022259a03ca36d2318
fumiaki.yoshikawa <fumiaki@sunpow-er.co.jp>
2018-12-22 08:28:24 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,7 +27,6 @@ class TasksController < ApplicationController
   end
   
   def update
-    binding.pry
     if @task.update(task_params)
       flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
