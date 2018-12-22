feb861e3665645505c9a8e763151688fcb278790
fumiaki.yoshikawa <fumiaki@sunpow-er.co.jp>
2018-12-22 05:33:15 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,6 +51,6 @@ def set_task
 end
 
 def task_params
-  params.require(:task).permit(:content)
+  params.require(:task).permit(:content,:status)
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181221082731) do
+ActiveRecord::Schema.define(version: 20181222051505) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "content"
+    t.string   "status"
   end
 
 end
